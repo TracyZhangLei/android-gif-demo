e918068551d07098a3222a3a4a8701ba1f4f7dbf
zhanglei <760796714@qq.com>
2014-04-05 21:10:59 +0800

    fix bug

--- src/com/tracyzhang/xgif/adapter/ChatAdapter.java
+++ src/com/tracyzhang/xgif/adapter/ChatAdapter.java
@@ -1,15 +1,8 @@
 package com.tracyzhang.xgif.adapter;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.tracyzhang.xgif.R;
-import com.tracyzhang.xgif.util.FaceManager;
-import com.tracyzhang.xgif.view.AnimatedGifDrawable;
-import com.tracyzhang.xgif.view.AnimatedImageSpan;
 
 import android.content.Context;
 import android.text.SpannableString;
@@ -19,6 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
+import com.tracyzhang.xgif.R;
+import com.tracyzhang.xgif.util.FaceManager;
+import com.tracyzhang.xgif.view.AnimatedGifDrawable;
+import com.tracyzhang.xgif.view.AnimatedImageSpan;
 
 
 /**
@@ -32,7 +30,6 @@ public class ChatAdapter extends BaseAdapter{
 	
 	private Pattern EMOTION_URL = Pattern.compile("\\[(\\S+?)\\]");
 	private FaceManager fm;
-	private Map<String,SpannableString> cacheMap;//cache
 	private Context cxt;
 	private List<String> data;
 
@@ -41,7 +38,6 @@ public class ChatAdapter extends BaseAdapter{
 		this.cxt = cxt;
 		this.data = data;
 		fm = FaceManager.getInstance();
-		cacheMap = new HashMap<String,SpannableString>();
 	}
 
 	@Override
@@ -87,25 +83,22 @@ public class ChatAdapter extends BaseAdapter{
 	 * @return
 	 */
 	private SpannableString convertNormalStringToSpannableString(String message , final TextView tv) {
-		SpannableString value = cacheMap.get(message);
-		if(null == value){
-			value = SpannableString.valueOf(message);
-			Matcher localMatcher = EMOTION_URL.matcher(value);
-			while (localMatcher.find()) {
-				String str2 = localMatcher.group(0);
-				int k = localMatcher.start();
-				int m = localMatcher.end();
-				if (m - k < 8) {
-					int face = fm.getFaceId(str2);
-					if(-1!=face){
-						AnimatedImageSpan localImageSpan = new AnimatedImageSpan(new AnimatedGifDrawable(cxt.getResources().openRawResource(face), new AnimatedGifDrawable.UpdateListener() {   
-						    @Override
-						    public void update() {//update the textview
-						        tv.postInvalidate();
-						    }
-						}));
-						value.setSpan(localImageSpan, k, m, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-					}
+		SpannableString value = SpannableString.valueOf(message);
+		Matcher localMatcher = EMOTION_URL.matcher(value);
+		while (localMatcher.find()) {
+			String str2 = localMatcher.group(0);
+			int k = localMatcher.start();
+			int m = localMatcher.end();
+			if (m - k < 8) {
+				int face = fm.getFaceId(str2);
+				if(-1!=face){
+					AnimatedImageSpan localImageSpan = new AnimatedImageSpan(new AnimatedGifDrawable(cxt.getResources().openRawResource(face), new AnimatedGifDrawable.UpdateListener() {   
+					    @Override
+					    public void update() {//update the textview
+					        tv.postInvalidate();
+					    }
+					}));
+					value.setSpan(localImageSpan, k, m, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
 				}
 			}
 		}
